62ba106ff185627de72f2e41cfce23f081c9f6b5
L K VAIBHAV <vvsvstuc086y16@vviedu.in>
2026-05-09 22:29:47 +0530

    Add boot.asm for x86_64 architecture initialization

---
+++ src/arch/x86_64/boot.asm
@@ -0,0 +1,21 @@
+bits 64
+default rel
+
+global _start
+extern kmain
+
+section .text
+_start:
+    mov rsp, stack_top
+    xor rbp, rbp
+    call kmain
+
+.hang:
+    hlt
+    jmp .hang
+
+section .bss
+align 16
+stack_bottom:
+    resb 16384
+stack_top:
