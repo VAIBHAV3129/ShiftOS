d289779f43d0454156adecbe3a7fc64d4f73e4cd
L K VAIBHAV <vvsvstuc086y16@vviedu.in>
2026-05-10 21:58:16 +0530

    Add limine.h header file with structures

---
+++ include/limine.h
@@ -0,0 +1,50 @@
+#ifndef SHIFTOS_LIMINE_H
+#define SHIFTOS_LIMINE_H
+
+#include "types.h"
+
+struct limine_uuid {
+    u64 a;
+    u64 b;
+};
+
+struct limine_base_revision_response {
+    u64 revision;
+};
+
+struct limine_base_revision_request {
+    struct limine_uuid id;
+    u64 revision;
+    struct limine_base_revision_response *response;
+};
+
+struct limine_framebuffer {
+    u64 address;
+    u64 width;
+    u64 height;
+    u64 pitch;
+    u16 bpp;
+    u8  memory_model;
+    u8  red_mask_size;
+    u8  red_mask_shift;
+    u8  green_mask_size;
+    u8  green_mask_shift;
+    u8  blue_mask_size;
+    u8  blue_mask_shift;
+    u8  unused[7];
+    u64 edid_size;
+    void *edid;
+};
+
+struct limine_framebuffer_response {
+    u64 framebuffer_count;
+    struct limine_framebuffer **framebuffers;
+};
+
+struct limine_framebuffer_request {
+    struct limine_uuid id;
+    u64 revision;
+    struct limine_framebuffer_response *response;
+};
+
+#endif
