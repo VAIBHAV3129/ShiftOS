30b384891e0d4d2d8c2aad7415dc462722226ae6
L K VAIBHAV <vvsvstuc086y16@vviedu.in>
2026-05-10 13:00:43 +0530

    Reference kernel memory sections in kmain
    Added kernel memory section references in kmain.

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,8 +1,20 @@
 #include "types.h"
 #include "compiler.h"
+#include "boot.h"
 
 SHIFTOS_NORETURN SHIFTOS_CALL
 void kmain(void) {
+    (void)&__kernel_start;
+    (void)&__kernel_end;
+    (void)&__text_start;
+    (void)&__text_end;
+    (void)&__rodata_start;
+    (void)&__rodata_end;
+    (void)&__data_start;
+    (void)&__data_end;
+    (void)&__bss_start;
+    (void)&__bss_end;
+
     for (;;) {
         __asm__ __volatile__("hlt");
     }
