b15728f60b3aec9bc036479a98b0fdb24c572b20
L K VAIBHAV <vvsvstuc086y16@vviedu.in>
2026-05-10 13:01:57 +0530

    Define kernel memory layout in boot.h header

---
+++ include/boot.h
@@ -0,0 +1,21 @@
+#ifndef SHIFTOS_BOOT_H
+#define SHIFTOS_BOOT_H
+
+#include "types.h"
+
+extern u8 __kernel_start;
+extern u8 __kernel_end;
+
+extern u8 __text_start;
+extern u8 __text_end;
+
+extern u8 __rodata_start;
+extern u8 __rodata_end;
+
+extern u8 __data_start;
+extern u8 __data_end;
+
+extern u8 __bss_start;
+extern u8 __bss_end;
+
+#endif
