f1c0217a490ec8e9eea891857652cbc2314fda98
L K VAIBHAV <vvsvstuc086y16@vviedu.in>
2026-05-10 09:28:54 +0530

    Add compiler attribute definitions in compiler.h

---
+++ include/compiler.h
@@ -0,0 +1,15 @@
+#ifndef SHIFTOS_COMPILER_H
+#define SHIFTOS_COMPILER_H
+
+#define SHIFTOS_NORETURN  __attribute__((noreturn))
+#define SHIFTOS_USED      __attribute__((used))
+#define SHIFTOS_PACKED    __attribute__((packed))
+#define SHIFTOS_ALIGNED(x) __attribute__((aligned(x)))
+#define SHIFTOS_SECTION(x) __attribute__((section(x)))
+#define SHIFTOS_SYSV      __attribute__((sysv_abi))
+#define SHIFTOS_CALL      __attribute__((sysv_abi))
+#define SHIFTOS_UNUSED    __attribute__((unused))
+
+#define SHIFTOS_INLINE static inline __attribute__((always_inline))
+
+#endif
