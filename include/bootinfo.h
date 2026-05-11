2a5243bf7f1d6461394553cf250c88d6d65bb9da
L K VAIBHAV <vvsvstuc086y16@vviedu.in>
2026-05-11 18:31:51 +0530

    Add framebuffer and boot_info structures in bootinfo.h
    Define framebuffer_info and boot_info structures for boot process.

--- include/bootinfo.h
+++ include/bootinfo.h
@@ -1,1 +1,30 @@
+#ifndef SHIFTOS_BOOTINFO_H
+#define SHIFTOS_BOOTINFO_H
 
+#include "types.h"
+#include "compiler.h"
+
+struct framebuffer_info {
+    u64 address;
+    u64 width;
+    u64 height;
+    u64 pitch;
+    u16 bpp;
+    u8  memory_model;
+    u8  red_mask_size;
+    u8  red_mask_shift;
+    u8  green_mask_size;
+    u8  green_mask_shift;
+    u8  blue_mask_size;
+    u8  blue_mask_shift;
+};
+
+struct boot_info {
+    int has_framebuffer;
+    struct framebuffer_info fb;
+};
+
+SHIFTOS_CALL int bootinfo_init(void);
+SHIFTOS_CALL const struct boot_info *bootinfo_get(void);
+
+#endif
