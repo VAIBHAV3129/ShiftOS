1ca26ea0b65d877b4a45bd684b8e8f74112d7d65
L K VAIBHAV <vvsvstuc086y16@vviedu.in>
2026-05-13 22:04:40 +0530

    Add colour.h header file with color functions

---
+++ include/colour.h
@@ -0,0 +1,11 @@
+#ifndef SHIFTOS_COLOR_H
+#define SHIFTOS_COLOR_H
+
+#include "types.h"
+#include "compiler.h"
+
+SHIFTOS_CALL u32 color_rgb(u8 r, u8 g, u8 b);
+SHIFTOS_CALL u32 color_rgba(u8 r, u8 g, u8 b, u8 a);
+SHIFTOS_CALL u32 color_blend(u32 src, u32 dst);
+
+#endif
