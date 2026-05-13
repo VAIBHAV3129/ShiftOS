496b5a55bd0c747771bfc022d5077db631c9225d
L K VAIBHAV <vvsvstuc086y16@vviedu.in>
2026-05-13 22:05:58 +0530

    Add alpha support for pixel and rectangle drawing

--- include/graphics.h
+++ include/graphics.h
@@ -15,7 +15,9 @@ struct gfx_context {
 SHIFTOS_CALL int gfx_init(void);
 SHIFTOS_CALL void gfx_clear(u32 color);
 SHIFTOS_CALL void gfx_put_pixel(u64 x, u64 y, u32 color);
+SHIFTOS_CALL void gfx_put_pixel_alpha(u64 x, u64 y, u32 color);
 SHIFTOS_CALL void gfx_fill_rect(u64 x, u64 y, u64 w, u64 h, u32 color);
+SHIFTOS_CALL void gfx_fill_rect_alpha(u64 x, u64 y, u64 w, u64 h, u32 color);
 SHIFTOS_CALL void gfx_draw_line(u64 x0, u64 y0, u64 x1, u64 y1, u32 color);
 SHIFTOS_CALL void gfx_draw_rect(u64 x, u64 y, u64 w, u64 h, u32 color);
 
