5ce5fbb5a4911e635a28e89ff880d334e6fd514a
L K VAIBHAV <vvsvstuc086y16@vviedu.in>
2026-05-16 18:21:41 +0530

    Update limine request IDs with new values

--- src/kernel/limine.c
+++ src/kernel/limine.c
@@ -3,14 +3,20 @@
 
 SHIFTOS_USED SHIFTOS_SECTION(".limine_reqs")
 volatile struct limine_base_revision_request limine_base_revision_request = {
-    .id = {0, 0},
+    .id = {
+        .a = 0xf759957357787220,
+        .b = 0x151486b70936130d
+    },
     .revision = 0,
     .response = 0
 };
 
 SHIFTOS_USED SHIFTOS_SECTION(".limine_reqs")
 volatile struct limine_framebuffer_request limine_framebuffer_request = {
-    .id = {0, 0},
+    .id = {
+        .a = 0x97127d0ddde8a74b,
+        .b = 0x6eac34d561d34eb4
+    },
     .revision = 0,
     .response = 0
 };
