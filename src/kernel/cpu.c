76a594e9eec376ca2685589124f2e33a83c3ed7a
L K VAIBHAV <vvsvstuc086y16@vviedu.in>
2026-05-16 19:20:45 +0530

    Add I/O port read/write functions and PIC remapping

--- src/kernel/cpu.c
+++ src/kernel/cpu.c
@@ -5,6 +5,45 @@ extern void irq0_handler_stub(void);
 static struct idt_entry g_idt[256];
 static struct idt_ptr g_idt_ptr;
 
+static inline void outb(u16 port, u8 val) {
+    __asm__ volatile("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+static inline u8 inb(u16 port) {
+    u8 ret;
+    __asm__ volatile("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
+
+static inline void io_wait(void) {
+    __asm__ volatile("outb %0, $0x80" : : "a"(0));
+}
+
+SHIFTOS_CALL void cpu_pic_remap(void) {
+    u8 a1 = inb(0x21);
+    u8 a2 = inb(0xA1);
+
+    outb(0x20, 0x11);
+    io_wait();
+    outb(0xA0, 0x11);
+    io_wait();
+    outb(0x21, 0x20);
+    io_wait();
+    outb(0xA1, 0x28);
+    io_wait();
+    outb(0x21, 0x04);
+    io_wait();
+    outb(0xA1, 0x02);
+    io_wait();
+    outb(0x21, 0x01);
+    io_wait();
+    outb(0xA1, 0x01);
+    io_wait();
+
+    outb(0x21, a1);
+    outb(0xA1, a2);
+}
+
 SHIFTOS_CALL void idt_set_gate(u8 vector, u64 handler_address, u16 selector, u8 attributes) {
     struct idt_entry *entry = &g_idt[vector];
     entry->offset_low = (u16)(handler_address & 0xFFFF);
