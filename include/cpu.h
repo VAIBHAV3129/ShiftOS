5ba4015a47c44d834552ffcc71d368eecc12f122
L K VAIBHAV <vvsvstuc086y16@vviedu.in>
2026-05-16 18:26:23 +0530

    Define IDT structures and functions in cpu.h
    Added IDT entry and pointer structures along with related functions.

--- include/cpu.h
+++ include/cpu.h
@@ -1,6 +1,7 @@
 #ifndef SHIFTOS_CPU_H
 #define SHIFTOS_CPU_H
 
+#include "types.h"
 #include "compiler.h"
 
 SHIFTOS_INLINE void cpu_hlt(void) {
@@ -14,5 +15,23 @@ SHIFTOS_INLINE void cpu_cli(void) {
 SHIFTOS_INLINE void cpu_sti(void) {
     __asm__ __volatile__("sti");
 }
+
+struct idt_entry {
+    u16 offset_low;
+    u16 selector;
+    u8 ist;
+    u8 attributes;
+    u16 offset_mid;
+    u32 offset_high;
+    u32 reserved;
+} __attribute__((packed));
+
+struct idt_ptr {
+    u16 limit;
+    u64 base;
+} __attribute__((packed));
+
+SHIFTOS_CALL void idt_set_gate(u8 vector, u64 handler_address, u16 selector, u8 attributes);
+SHIFTOS_CALL void idt_init(void);
 
 #endif
