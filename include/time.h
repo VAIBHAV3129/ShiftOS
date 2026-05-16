2ceb3eb2e5a691148738e75ac70d1fa7558db3d2
L K VAIBHAV <vvsvstuc086y16@vviedu.in>
2026-05-16 18:27:16 +0530

    Add PIT initialization and handler callback functions

--- include/time.h
+++ include/time.h
@@ -7,5 +7,7 @@
 SHIFTOS_CALL void time_init(void);
 SHIFTOS_CALL u64 time_ticks(void);
 SHIFTOS_CALL void time_wait(u64 ticks);
+SHIFTOS_CALL void pit_init(u32 frequency);
+SHIFTOS_CALL void pit_handler_callback(void);
 
 #endif
