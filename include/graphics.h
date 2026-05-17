c89125694d26586e305a7d057b16f33bc92d6ee8
L K VAIBHAV <vvsvstuc086y16@vviedu.in>
2026-05-17 18:46:00 +0530

    Refactor graphics.h function signatures and parameters

--- include/graphics.h
+++ include/graphics.h
@@ -22,19 +22,15 @@ SHIFTOS_CALL void gfx_put_pixel(u64 x, u64 y, u32 color);
 SHIFTOS_CALL void gfx_put_pixel_alpha(u64 x, u64 y, u32 color);
 SHIFTOS_CALL void gfx_fill_rect(u64 x, u64 y, u64 w, u64 h, u32 color);
 SHIFTOS_CALL void gfx_fill_rect_alpha(u64 x, u64 y, u64 w, u64 h, u32 color);
-SHIFTOS_CALL void gfx_draw_line(u64 x0, u64 y0, u64 x1, u64 y1, u32 color);
 SHIFTOS_CALL void gfx_draw_rect(u64 x, u64 y, u64 w, u64 h, u32 color);
-SHIFTOS_CALL void gfx_draw_gradient(u64 x, u64 y, u64 w, u64 h, u32 top, u32 bottom);
+SHIFTOS_CALL void gfx_draw_gradient(u64 x, u64 y, u64 w, u64 h, u32 color_top, u32 color_bottom);
 SHIFTOS_CALL void gfx_draw_checker(u64 size, u32 c1, u32 c2);
 
-SHIFTOS_CALL void gfx_draw_circle(u64 cx, u64 cy, u64 r, u32 color);
-SHIFTOS_CALL void gfx_fill_circle(u64 cx, u64 cy, u64 r, u32 color);
-
 SHIFTOS_CALL void gfx_draw_text(u64 x, u64 y, const char *text, u32 color);
-SHIFTOS_CALL void gfx_draw_text_glow(u64 x, u64 y, const char *text, u32 color, u32 glow);
-SHIFTOS_CALL void gfx_measure_text(const char *text, u64 *out_w, u64 *out_h);
+SHIFTOS_CALL void gfx_draw_text_glow(u64 x, u64 y, const char *text, u32 color, u32 glow_color);
+SHIFTOS_CALL void gfx_measure_text(const char *text, u64 *w, u64 *h);
 
-SHIFTOS_CALL void gfx_draw_window(u64 x, u64 y, u64 w, u64 h, const char *title, u32 accent, u32 body);
+SHIFTOS_CALL void gfx_draw_window(u64 x, u64 y, u64 w, u64 h, const char *title, u32 accent, u32 bg);
 SHIFTOS_CALL void gfx_draw_textf(u64 x, u64 y, u32 color, const char *label, u64 value);
 
 #endif
