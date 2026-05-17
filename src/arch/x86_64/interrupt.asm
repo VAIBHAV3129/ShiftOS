ed09393f626e52b01b97aca47dfcecfad666055a
L K VAIBHAV <vvsvstuc086y16@vviedu.in>
2026-05-17 12:36:06 +0530

    Add additional register pushes and pops in interrupt.asm

--- src/arch/x86_64/interrupt.asm
+++ src/arch/x86_64/interrupt.asm
@@ -12,9 +12,23 @@ irq0_handler_stub:
     push r9
     push r10
     push r11
+    push rbx
+    push rbp
+    push r12
+    push r13
+    push r14
+    push r15
 
+    sub rsp, 8
     call pit_handler_callback
+    add rsp, 8
 
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop rbp
+    pop rbx
     pop r11
     pop r10
     pop r9
