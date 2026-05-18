8904e07e99912ab3fa80866c69df17cfb7665f6c
L K VAIBHAV <vvsvstuc086y16@vviedu.in>
2026-05-18 10:14:13 +0530

    Refactor kpanic declaration in panic.h

--- include/panic.h
+++ include/panic.h
@@ -1,9 +1,5 @@
 #ifndef SHIFTOS_PANIC_H
 #define SHIFTOS_PANIC_H
-
 #include "compiler.h"
-
-SHIFTOS_NORETURN SHIFTOS_CALL
-void kpanic(const char *msg);
-
+SHIFTOS_NORETURN SHIFTOS_CALL void kpanic(const char *msg);
 #endif
