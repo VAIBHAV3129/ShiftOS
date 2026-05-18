43e1ed12399829c184eab38c5ac2a2ea2d677e4d
L K VAIBHAV <vvsvstuc086y16@vviedu.in>
2026-05-18 10:15:17 +0530

    Refactor inline functions and struct definitions

--- include/cpu.h
+++ include/cpu.h
@@ -1,38 +1,16 @@
 #ifndef SHIFTOS_CPU_H
 #define SHIFTOS_CPU_H
-
 #include "types.h"
 #include "compiler.h"
-
-SHIFTOS_INLINE void cpu_hlt(void) {
-    __asm__ __volatile__("hlt");
-}
-
-SHIFTOS_INLINE void cpu_cli(void) {
-    __asm__ __volatile__("cli");
-}
-
-SHIFTOS_INLINE void cpu_sti(void) {
-    __asm__ __volatile__("sti");
-}
-
+SHIFTOS_INLINE void cpu_hlt(void) { __asm__ __volatile__("hlt"); }
+SHIFTOS_INLINE void cpu_cli(void) { __asm__ __volatile__("cli"); }
+SHIFTOS_INLINE void cpu_sti(void) { __asm__ __volatile__("sti"); }
 struct idt_entry {
-    u16 offset_low;
-    u16 selector;
-    u8 ist;
-    u8 attributes;
-    u16 offset_mid;
-    u32 offset_high;
-    u32 reserved;
+    u16 offset_low; u16 selector; u8 ist; u8 attributes;
+    u16 offset_mid; u32 offset_high; u32 reserved;
 } __attribute__((packed));
-
-struct idt_ptr {
-    u16 limit;
-    u64 base;
-} __attribute__((packed));
-
+struct idt_ptr { u16 limit; u64 base; } __attribute__((packed));
 SHIFTOS_CALL void idt_set_gate(u8 vector, u64 handler_address, u16 selector, u8 attributes);
 SHIFTOS_CALL void idt_init(void);
 SHIFTOS_CALL void cpu_pic_remap(void);
-
 #endif
